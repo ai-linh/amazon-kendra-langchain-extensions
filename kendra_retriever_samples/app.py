9058b354e084c6270ac3a8036181e5ee8c602013
Ai-Linh Le <ailinhle@amazon.com>
2023-07-09 23:13:28 +1000

    added Falcon 40B

--- kendra_retriever_samples/app.py
+++ kendra_retriever_samples/app.py
@@ -48,24 +48,20 @@ else:
     user_id = str(uuid.uuid4())
     st.session_state['user_id'] = user_id
 
-
 if 'llm_chain' not in st.session_state:
-    if (len(sys.argv) > 1):
-        if (sys.argv[1] == 'anthropic'):
+    if len(sys.argv) > 1:
+        if sys.argv[1] == 'anthropic':
             st.session_state['llm_app'] = anthropic
             st.session_state['llm_chain'] = anthropic.build_chain()
-        elif (sys.argv[1] == 'flanxl'):
+        elif sys.argv[1] == 'flanxl':
             st.session_state['llm_app'] = flanxl
             st.session_state['llm_chain'] = flanxl.build_chain()
-        elif (sys.argv[1] == 'flanxxl'):
+        elif sys.argv[1] == 'flanxxl':
             st.session_state['llm_app'] = flanxxl
             st.session_state['llm_chain'] = flanxxl.build_chain()
-        elif (sys.argv[1] == 'openai'):
+        elif sys.argv[1] == 'openai':
             st.session_state['llm_app'] = openai
             st.session_state['llm_chain'] = openai.build_chain()
-        elif (sys.argv[1] == 'falcon40b'):
-            st.session_state['llm_app'] = falcon40b
-            st.session_state['llm_chain'] = falcon40b.build_chain()
         elif (sys.argv[1] == 'llama2'):
             st.session_state['llm_app'] = llama2
             st.session_state['llm_chain'] = llama2.build_chain()
@@ -78,20 +74,23 @@ if 'llm_chain' not in st.session_state:
         elif (sys.argv[1] == 'bedrock_claudev2'):
             st.session_state['llm_app'] = bedrock_claudev2
             st.session_state['llm_chain'] = bedrock_claudev2.build_chain()
-        elif (sys.argv[1] == 'j2_mid'):
+        elif sys.argv[1] == 'j2_mid':
             st.session_state['llm_app'] = j2_mid
             st.session_state['llm_chain'] = j2_mid.build_chain()
-        elif (sys.argv[1] == 'j2_ultra'):
+        elif sys.argv[1] == 'j2_ultra':
             st.session_state['llm_app'] = j2_ultra
             st.session_state['llm_chain'] = j2_ultra.build_chain()
+        elif sys.argv[1] == 'falcon40b':
+            st.session_state['llm_app'] = falcon40b
+            st.session_state['llm_chain'] = falcon40b.build_chain()
         else:
             raise Exception("Unsupported LLM: ", sys.argv[1])
     else:
-        raise Exception("Usage: streamlit run app.py <anthropic|flanxl|flanxxl|openai|bedrock_titan|bedrock_claude|bedrock|claudev2||j2_mid|j2_ultra>")
+        raise Exception("Usage: streamlit run app.py <anthropic|flanxl|flanxxl|openai|bedrock_titan|bedrock_claude|bedrock|claudev2||j2_mid|j2_ultra|falcon40b>")
 
 if 'chat_history' not in st.session_state:
     st.session_state['chat_history'] = []
-    
+
 if "chats" not in st.session_state:
     st.session_state.chats = [
         {
@@ -109,7 +108,6 @@ if "answers" not in st.session_state:
 
 if "input" not in st.session_state:
     st.session_state.input = ""
-
 
 st.markdown("""
         <style>
@@ -129,10 +127,11 @@ st.markdown("""
         </style>
         """, unsafe_allow_html=True)
 
+
 def write_logo():
     col1, col2, col3 = st.columns([5, 1, 5])
     with col2:
-        st.image(AI_ICON, use_column_width='always') 
+        st.image(AI_ICON, use_column_width='always')
 
 
 def write_top_bar():
@@ -151,6 +150,7 @@ def write_top_bar():
         clear = st.button("Clear Chat")
     return clear
 
+
 clear = write_top_bar()
 
 if clear:
@@ -158,6 +158,7 @@ if clear:
     st.session_state.answers = []
     st.session_state.input = ""
     st.session_state["chat_history"] = []
+
 
 def handle_input():
     input = st.session_state.input
@@ -190,6 +191,7 @@ def handle_input():
     })
     st.session_state.input = ""
 
+
 def write_user_message(md):
     col1, col2 = st.columns([1,12])
     
@@ -209,12 +211,14 @@ def render_result(result):
         else:
             render_sources([])
 
+
 def render_answer(answer):
     col1, col2 = st.columns([1,12])
     with col1:
         st.image(AI_ICON, use_column_width='always')
     with col2:
         st.info(answer['answer'])
+
 
 def render_sources(sources):
     col1, col2 = st.columns([1,12])
@@ -223,19 +227,19 @@ def render_sources(sources):
             for s in sources:
                 st.write(s)
 
-    
-#Each answer will have context of the question asked in order to associate the provided feedback with the respective question
+
+# Each answer will have context of the question asked in order to associate the provided feedback with the respective question
 def write_chat_message(md, q):
     chat = st.container()
     with chat:
         render_answer(md['answer'])
         render_sources(md['sources'])
-    
-        
+
+
 with st.container():
-  for (q, a) in zip(st.session_state.questions, st.session_state.answers):
-    write_user_message(q)
-    write_chat_message(a, q)
+    for (q, a) in zip(st.session_state.questions, st.session_state.answers):
+        write_user_message(q)
+        write_chat_message(a, q)
 
 st.markdown('---')
 input = st.text_input("You are talking to an AI, ask any question.", key="input", on_change=handle_input)
