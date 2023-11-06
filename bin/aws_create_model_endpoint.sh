ded605dc2d2aafbb032d180a50f98f893d4d11ce
Ai-Linh Le <ailinhle@amazon.com>
2023-11-06 14:02:43 +1100

    added script to create model endpoint

---
+++ bin/aws_create_model_endpoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+display_usage() {
+	echo -e "Usage: $0 falcon40b|llama2|j2_ultra\n"
+}
+
+if [[ $# -lt 1 ]]
+then
+  display_usage
+  exit 1
+fi
+
+MODEL_NAME=$1
+ENDPOINT_NAME="hf-llm-falcon-40b-instruct-bf16-2023-08-01-09-12-18-671"
+
+if [[ $MODEL_NAME == "falcon40b" ]]; then
+     ENDPOINT_NAME="hf-llm-falcon-40b-instruct-bf16-2023-08-01-09-12-18-671"
+elif [[ $MODEL_NAME == "llama2" ]]; then
+     ENDPOINT_NAME="jumpstart-dft-meta-textgeneration-llama-2-70b-f"
+elif [[ $MODEL_NAME == "j2_ultra" ]]; then
+     ENDPOINT_NAME="j2-ultra"
+fi
+
+echo "Creating endpoint: $ENDPOINT_NAME"
+
+aws sagemaker create-endpoint --endpoint-name "$ENDPOINT_NAME" --endpoint-config-name "$ENDPOINT_NAME" --region us-east-1
